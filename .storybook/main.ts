0d62078d450c6379b719a6d73ec5f1ffcf37ec5e
Jocelin Queau <queau.j@gmail.com>
2023-07-22 13:53:23 +0200

    fix svg img link, import new package
    storybook 7.1
    react router and react router dom

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,25 +1,13 @@
-import path from 'path';
-
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
-    '@storybook/addon-storysource',
   ],
-  viteFinal: async (config, { _configType }) => {
-    // Add your custom aliases to the resolve.alias property
-    config.resolve = config.resolve ?? {};
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, '../src/'),
-    };
-    // Return the updated configuration
-    return config;
-  },
   framework: {
     name: '@storybook/react-vite',
     options: {},
